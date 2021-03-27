1e213a3de5785ff6918922411725d1473a521a80
himIoT19 <thehimu1990@gmail.com>
2021-03-27 22:55:43 +0530

    TODO: Query parameters

--- main.py
+++ main.py
@@ -9,6 +9,18 @@ def index():
     return {'data': {'name': 'Himanshu'}}  # 'Hi Himanshu'
 
 
-@app.get('/about')
-def about():
-    return {'data': 'about page'}
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data': 'unpublished'}
+
+
+@app.get('/blog/{id}')
+def show(id: int):  # if we don't use :int we will get string in the output
+    # Fetch blog with id = '{id}'
+    return {'data': id}
+
+
+@app.get('/blog/{id}/comments')
+def comments(id: int):
+    # Fetch comments of blog with id = '{id}'
+    return {'data': {'comment1', 'comment2', 'comment3'}}
