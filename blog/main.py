f5d273ea2963c991363db57bd9170ae46b14bf99
himIoT19 <thehimu1990@gmail.com>
2021-03-28 19:23:10 +0530

    TODO: Get blogs from the database

--- blog/main.py
+++ blog/main.py
@@ -1,12 +1,27 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from . import schemas, models
-from .database import engine
+from .database import engine, SessionLocal
+from sqlalchemy.orm import Session
 
 models.Base.metadata.create_all(engine)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 
 app = FastAPI()
 
 
 @app.post('/blog/')
-def create(request: schemas.Blog):
-    return request
+def create(request: schemas.Blog, db: Session = Depends(get_db)):
+    # Create a new blog
+    new_blog = models.Blog(title=request.title, body=request.body)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
